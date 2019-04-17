f2ff6ae638cc85dd67019c28182290d1c23636ca
gosia <gosiaak77@gmail.com>
2019-04-17 19:50:48 +0200

    initial commit

---
+++ src/main/java/pl/mnastaly/brightlightjurnal/BrightlightjurnalApplication.java
@@ -0,0 +1,13 @@
+package pl.mnastaly.brightlightjurnal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrightlightjurnalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BrightlightjurnalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/mnastaly/brightlightjurnal/BrightlightjurnalApplicationTests.java
@@ -0,0 +1,16 @@
+package pl.mnastaly.brightlightjurnal;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BrightlightjurnalApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
