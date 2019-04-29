deafac5147380f3a168023ddc8c50a72f96e24fa
Małgorzata Nastały <malgorzata.nastaly@avra.pl>
2019-04-29 16:31:53 +0200

    create many to many relation between meal and ingredient

--- src/main/java/pl/mnastaly/brightlightjurnal/ingredient/Ingredient.java
+++ src/main/java/pl/mnastaly/brightlightjurnal/ingredient/Ingredient.java
@@ -1,10 +1,14 @@
 package pl.mnastaly.brightlightjurnal.ingredient;
 
+import java.util.List;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
 
 import lombok.Builder;
@@ -23,4 +27,7 @@ public class Ingredient {
     @JoinColumn(name = "id_product")
     private Product product;
     private Double grams;
+    @ManyToMany
+    @JoinTable(name = "meal_x_ingredient")
+    private List<Ingredient> ingredients;
 }
--- src/main/java/pl/mnastaly/brightlightjurnal/meal/Meal.java
+++ src/main/java/pl/mnastaly/brightlightjurnal/meal/Meal.java
@@ -1,11 +1,15 @@
 package pl.mnastaly.brightlightjurnal.meal;
 
+import java.util.List;
+
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 
 import lombok.Data;
 
@@ -20,4 +24,7 @@ public class Meal {
     String note;
     @Enumerated(EnumType.STRING)
     MealType mealType;
+    @ManyToMany
+    @JoinTable(name = "meal_x_ingredient")
+    private List<Meal> meals;
 }
